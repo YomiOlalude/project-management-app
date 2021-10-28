3b1c40954ae40d1fc51c91860f8feccb28167029
Yomi Olalude <yomi.olalude@gmail.com>
2021-10-28 10:59:27 +0100

    updated views

--- api/views.py
+++ api/views.py
@@ -10,22 +10,24 @@ from .forms import ProjectCreateForm
 
 # Create your views here.
 
+
 class ProjectsListView(LoginRequiredMixin, APIView):
     """
     List all projects, or create a new project.
     """
 
     def get(self, request, format=None):
-        projects = Project.objects.all()
+        projects = Project.objects.all(user=request.user)
         serializer = ProjectSerializer(projects, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-    def post (self, request, format=None):
+    def post(self, request, format=None):
         serializer = ProjectSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 class ProjectDetailView(LoginRequiredMixin, APIView):
     """
@@ -57,10 +59,14 @@ class ProjectDetailView(LoginRequiredMixin, APIView):
             return Response({}, status=status.HTTP_404_NOT_FOUND)
         project = project.filter(user=request.user)
         if not project.exists():
-            return Response({"message": "You cannot delete this Project"}, status=status.HTTP_404_NOT_FOUND)
+            return Response(
+                {"message": "You cannot delete this Project"},
+                status=status.HTTP_404_NOT_FOUND,
+            )
         obj = project.first()
         obj.delete()
         return Response({"message": "Project deleted"}, status=status.HTTP_200_OK)
+
 
 class ProjectCreateView(LoginRequiredMixin, APIView):
     """
@@ -75,18 +81,16 @@ class ProjectCreateView(LoginRequiredMixin, APIView):
             completed = form.cleaned_data["completed"]
             duration = form.cleaned_data["duration"]
             date_created = form.cleaned_data["date_created"]
-            project = Project(name=name, 
-                            user=self.request.user,
-                            description=description, 
-                            completed=completed, 
-                            duration=duration, 
-                            date_created=date_created)
+            project = Project(
+                name=name,
+                user=self.request.user,
+                description=description,
+                completed=completed,
+                duration=duration,
+                date_created=date_created,
+            )
             project.save()
             serializer = ProjectSerializer(project)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
         return Response({"message": "Invalid form"}, status=status.HTTP_400_BAD_REQUEST)
-
-
-
-
